8da2078113b6e6a817678a68748873816e018177
JosephAv <g.averta3@gmail.com>
2026-05-20 14:59:24 +0200

    aggiunta pesi, calcolo vicini, ricorsione

--- database/DAO.py
+++ database/DAO.py
@@ -43,3 +43,26 @@ class DAO():
         cursor.close()
         conn.close()
         return result
+
+    @staticmethod
+    def getSalariesTeam(year, idMapTeams):
+        conn = DBConnect.get_connection()
+
+        result = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = """select t.ID, t.teamCode, sum(s.salary) as totSalary
+                    from salaries s, teams t, appearances a 
+                    where s.`year` = t.`year` and t.`year` = a.`year` and a.`year` = %s
+                    and t.ID = a.teamID and a.playerID = s.playerID 
+                    group by t.ID , t.teamCode """
+
+        cursor.execute(query, (year, ))
+
+        mapSalary ={}
+        for row in cursor:
+             mapSalary[idMapTeams[ row["ID"]]] = row["totSalary"]
+
+        cursor.close()
+        conn.close()
+        return mapSalary
--- model/model.py
+++ model/model.py
@@ -1,4 +1,7 @@
+import copy
 import itertools
+import random
+from ipaddress import v4_int_to_packed
 
 import networkx as nx
 
@@ -9,8 +12,86 @@ class Model:
     def __init__(self):
         self._grafo = nx.Graph()
         self._teams = []
+        self._idMapTeams = None
+        self._bestPath = []
+        self._bestObjVal = 0
 
-    def creaGrafo(self):
+    def getPath(self, v0):
+        self._bestPath = []
+        self._bestObjVal = 0
+
+        parziale = [v0]
+
+        for v in self._grafo.neighbors(v0):
+            parziale.append(v)
+            self._ricorsione(parziale)
+            parziale.pop()
+
+    def getPathV2(self, v0):
+        self._bestPath = []
+        self._bestObjVal = 0
+
+        parziale = [v0]
+
+        listaVicini = self.getVicini(parziale[-1])
+        parziale.append(listaVicini[0][0])
+        self._ricorsioneV2(parziale)
+
+        return self._bestPath, self._bestObjVal
+
+
+
+    def _ricorsione(self, parziale):
+        print(len(parziale))
+        #1 condizione di ottimalità, verifico se la parziale è migliore del best
+        if self._score(parziale) > self._bestObjVal:
+            self._bestPath = copy.deepcopy(parziale)
+            self._bestObjVal = self._score(parziale)
+
+        #2 condizione di terminazione, verifico se posso continuare
+
+        #3 faccio la mia ricorsione
+        for v in self._grafo.neighbors(parziale[-1]):
+            pesoE = self._grafo[parziale[-1]][v]["weight"]
+
+            if self._grafo[parziale[-2]][parziale[-1]]["weight"] > pesoE and v not in parziale:
+                parziale.append(v)
+                self._ricorsione(parziale)
+                parziale.pop()
+
+    def _ricorsioneV2(self, parziale):
+        # 1 condizione di ottimalità, verifico se la parziale è migliore del best
+        if self._score(parziale) > self._bestObjVal:
+            self._bestPath = copy.deepcopy(parziale)
+            self._bestObjVal = self._score(parziale)
+
+        # 2 condizione di terminazione, verifico se posso continuare
+
+        # 3 faccio la mia ricorsione
+        # listaVicini = []
+        # for v in self._grafo.neighbors(parziale[-1]):
+        #     edgeV = self._grafo[parziale[-1]][v]["weight"]
+        #     listaVicini.append((v, edgeV))
+        #
+        # listaVicini.sort(key= lambda x: x[1], reverse=True)
+
+        listaVicini = self.getVicini(parziale[-1])
+
+        for v in listaVicini:
+            if v[0] not in parziale and self._grafo[parziale[-2]][parziale[-1]]["weight"] > v[1]:
+                parziale.append(v[0])
+                self._ricorsioneV2(parziale)
+                parziale.pop()
+                return
+
+    def _score(self, parziale):
+        score = 0
+        for i in range(0, len(parziale)-1):
+            score += self._grafo[parziale[i]][parziale[i+1]]["weight"]
+
+        return score
+
+    def creaGrafo(self, year):
         self._grafo.clear()
         self._grafo.add_nodes_from(self._teams)
 
@@ -22,8 +103,31 @@ class Model:
         myedges = list(itertools.combinations(self._teams, 2))
         self._grafo.add_edges_from(myedges)
 
+        mapSalary = DAO.getSalariesTeam(year, self._idMapTeams)
+        for e in self._grafo.edges:
+            sal1 = mapSalary[e[0]]
+            sal2 = mapSalary[e[1]]
+            peso = sal1+sal2
+            self._grafo[e[0]][e[1]]["weight"] = sal1+sal2
+
+            # self._grafo[e[0]][e[1]]["weight"] = mapSalary[e[0]] + mapSalary[e[1]]
+
+
+    def getVicini(self, source):
+        vicini =  self._grafo.neighbors(source)
+        viciniTuples = []
+        for v in vicini:
+            viciniTuples.append( (v, self._grafo[source][v]["weight"]) )
+
+        viciniTuples.sort(key= lambda x: x[1], reverse=True)
+
+        return viciniTuples
+
+
     def getTeamsOfYear(self, year):
         self._teams =  DAO.getTeamsOfYear(year)
+        self._idMapTeams = {t.ID : t for t in self._teams}
+
         return self._teams
 
     def getAllYears(self):
@@ -31,3 +135,7 @@ class Model:
 
     def getGraphDetails(self):
         return len(self._grafo.nodes), len(self._grafo.edges)
+
+    def getRandomNode(self):
+        index = random.randint(0, len(self._teams ))
+        return self._teams[index]
